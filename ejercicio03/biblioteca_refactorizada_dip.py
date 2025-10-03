9ae7ae5ab5eb51943f18ad013055b6fa780a48c8
Emilio Maciel <emilio.maciel@iteso.mx>
2025-10-03 17:49:14 -0600

    respuestas abiertas en el EXAMEN_SOLID_POO.md

--- ejercicio03/biblioteca_refactorizada_dip.py
+++ ejercicio03/biblioteca_refactorizada_dip.py
@@ -1,6 +1,6 @@
-from biblioteca_refactorizada_srp import SistemaBibliotecaSRP
-from ejercicio03_intrerfaces import IRepositorio
-from ejercicio2_srp import ValidadorBiblioteca, ServicioNotificaciones
+from EXAMEN_SOLID_POO.ejercicio02.biblioteca_refactorizada_srp import SistemaBibliotecaSRP
+from EXAMEN_SOLID_POO.ejercicio03.ejercicio03_intrerfaces import IRepositorio
+from EXAMEN_SOLID_POO.ejercicio02.ejercicio2_srp import ValidadorBiblioteca, ServicioNotificaciones
 
 
 class SistemaBibliotecaDIP(SistemaBibliotecaSRP):
